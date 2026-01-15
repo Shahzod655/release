8bca2a6634124f33f3fcf44df6850faf9a0de5a2
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2026-01-15 21:27:36 +0500

    п

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "222874038"
+    serviceWorkerVersion: "3898336315"
   }
 });
